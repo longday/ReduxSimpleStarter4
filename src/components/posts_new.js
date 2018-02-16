f6d78c928d1e2ef9133dc3a4ec4c7b1e9f98ea0d
longday <6600gm@gmail.com>
2018-02-16 18:48:00 +0300

    l 145

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,58 +1,81 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import {createPost} from "../actions";
 
 
 class PostsNew extends  React.Component {
+
     renderField(field){
+        const { meta:{touched, error}, label, input } = field;
+
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return(
-            <div className="form-group">
-                <label>{field.label}</label>
+            <div className={className}>
+                <label>{label}</label>
                 <input
                     className="form-control"
                     type="text"
-                    {...field.index}
+                    {...input}
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
 
-    onSubmit(values){
-
+    onSubmit = (values) => {
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render(){
-        const { handleSubmit } =  this.props;
-
         return(
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <Field name="title" label="Title For Post" component={this.renderField} />
-                <Field name="tags"  label="Tags" component={this.renderField} />
-                <Field name="content"  label="Post Content" component={this.renderField} />
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field
+                    name="title"
+                    label="Title For Post"
+                    component={this.renderField}
+                />
+                <Field
+                    name="categories"
+                    label="Categories"
+                    component={this.renderField}
+                />
+                <Field
+                    name="content"
+                    label="Post Content"
+                    component={this.renderField}
+                />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="\" className="btn btn-danger">Cancel</Link>
             </form>
-        )
+        );
     }
 }
 
 function validate (values) {
-    const errors ={};
-    if (!values.title || values.title.length < 3) {
-        errors.title = "Enter a title that is at least 3 characters!";
+    const errors = {};
+    if (!values.title) {
+        errors.title = "Enter a title!";
     }
-    if (!values.tags) {
-        errors.tags = "Enter some categories!";
+    if (!values.categories) {
+        errors.categories = "Enter some categories!";
     }
     if (!values.content) {
         errors.content = "Enter some content!";
     }
-
-
     return errors;
 }
 
 
 export default reduxForm({
-    validate: validate,
-    form: 'PostsNewForm'
-})(PostsNew);
+    form: 'PostsNewForm',
+    validate
+})(
+    connect(null, {createPost})(PostsNew)
+);
